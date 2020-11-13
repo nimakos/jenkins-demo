e26fccebe80b5c49ffb3933d03f00dc5894fd8b1
nimakos <nimakos@hotmail.com>
2020-11-13 13:32:17 +0200

    groovy

--- createJobs.groovy
+++ createJobs.groovy
@@ -1,3 +1,4 @@
+import jenkins.model.*
 /*Init job from jenkins*/
 pipelineJob('pipelineJob') {
     definition {
@@ -23,3 +24,14 @@ pipelineJob('spring-boot-job') {
         }
     }
 }
+
+println "Adding an auto installer for Maven 3.3.9"
+
+def mavenPluginExtension = Jenkins.instance.getExtensionList(hudson.tasks.Maven.DescriptorImpl.class)[0]
+
+def asList = (mavenPluginExtension.installations as List)
+asList.add(new hudson.tasks.Maven.MavenInstallation('maven-3', null, [new hudson.tools.InstallSourceProperty([new hudson.tasks.Maven.MavenInstaller("3.3.9")])]))
+
+mavenPluginExtension.installations = asList
+
+mavenPluginExtension.save()
